283aa20477d7846ee5d02af94e7f6d24700d8c74
Joss <a01664187@tec.mx>
2026-06-06 13:41:44 -0600

    Correcion

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include "episodio.h"
 #include "pelicula.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
+
 
 int main(){
 
+    int opcion;
+    do{
+    cout << "Menu:\n"
+         << "1. Cargar archivo de datos\n"
+         << "2. Mostrar los datos\n"
+         << "3. Mostrar los videos en general con una cierta calificacion o de un cierto género\n"
+         << "4. Mostrar los episodios de una dterminada serie con una calificacion determinada\n"
+         << "5. Mostrar las peliculas con cierta calificacion\n"
+         << "6. Calificar un video\n"
+         << "0. Salir\n"
+         << "Eige una opcion: ";
+        cin >> opcion;
+    
+    }while()
     return 0;
 }
